2477c178f79f9d8b6ee31695419476e42cfa3299
James Hager <james@avantiapplications.com>
2022-02-18 09:14:30 -0700

    Completed Part 1: Data Manipulation with Python

--- process.py
+++ process.py
@@ -1,12 +1,14 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt")  # open the txt file
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file):  # make the function sales_reports
+    for line in log_file:  # loop over all lines in the file
+        line = line.rstrip()  # remove trailing characters from the line
+        day = line[0:3]  # extract the day-of-the-week (the first 3 characters of the line)
+        if day == "Tue":  # if day is 'Tue'
+            print(line)  # print the whole line of data
 
 
-sales_reports(log_file)
+sales_reports(log_file)  # execute the sales_reports function on log_file
+
+log_file.close()  # close the log_file reader
