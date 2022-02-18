8af5385df75cd6215c7b61610e435cfaaf9cdd78
James Hager <james@avantiapplications.com>
2022-02-18 11:21:16 -0700

    Completed Python extra credit.

--- process.py
+++ process.py
@@ -9,6 +9,20 @@ def sales_reports(log_file):  # make the function sales_reports
             print(line)  # print the whole line of data
 
 
+def more_than_ten(log_file):  # make the function more_than_ten
+    for line in log_file:  # loop over all lines in the file
+        line = line.rstrip()  # remove trailing characters from the line
+        elements = line.split(' ')  # split the line at spaces
+        if int(elements[2]) > 10:  # if number sold > 10
+            print(line)  # print the whole line of data
+
+
+print("===\n====== sales_reports =========================================\n===")  # print out a separator
 sales_reports(log_file)  # execute the sales_reports function on log_file
+
+log_file.seek(0,0)  # rewind the file
+
+print("===\n====== more_than_ten =========================================\n===")  # print out a separator
+more_than_ten(log_file)  # execute the more_than_ten function on log_file
 
 log_file.close()  # close the log_file reader
